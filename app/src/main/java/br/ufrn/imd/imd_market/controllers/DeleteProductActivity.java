01e7bff714ac22cc7b64560211e9d1ffd6676901
Hugo Santos <hf.santos2332@gmail.com>
2023-11-15 20:21:01 -0300

    Cria tela de remover produto

---
+++ app/src/main/java/br/ufrn/imd/imd_market/controllers/DeleteProductActivity.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd.imd_market.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufrn.imd.imd_market.R;
+
+public class DeleteProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_product);
+    }
+}
