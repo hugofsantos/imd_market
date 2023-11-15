4924cfc6053b2d1d28303c7dbb62503dfb833af7
Hugo Santos <hf.santos2332@gmail.com>
2023-11-15 20:27:23 -0300

    Cria tela de listagem de produtos

---
+++ app/src/main/java/br/ufrn/imd/imd_market/controllers/ListProductsActivity.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd.imd_market.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufrn.imd.imd_market.R;
+
+public class ListProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_products);
+    }
+}
