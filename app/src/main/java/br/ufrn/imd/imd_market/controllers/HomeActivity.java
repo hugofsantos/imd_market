e6b796b6840a2eccd602d83980403641facb2d8b
Hugo Santos <hf.santos2332@gmail.com>
2023-11-17 14:57:09 -0300

    Implementa navegação na página home

--- app/src/main/java/br/ufrn/imd/imd_market/controllers/HomeActivity.java
+++ app/src/main/java/br/ufrn/imd/imd_market/controllers/HomeActivity.java
@@ -2,7 +2,10 @@ package br.ufrn.imd.imd_market.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.ufrn.imd.imd_market.R;
 
@@ -12,5 +15,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button createProduct = (Button) findViewById(R.id.createProductNav);
+        Button listProducts = (Button) findViewById(R.id.listProductsNav);
+        Button updateProduct = (Button) findViewById(R.id.updateProductNav);
+        Button deleteProduct = (Button) findViewById(R.id.deleteProductNav);
+
+        createProduct.setOnClickListener(navigateTo(CreateProductActivity.class));
+        listProducts.setOnClickListener(navigateTo(ListProductsActivity.class));
+        updateProduct.setOnClickListener(navigateTo(UpdateProductActivity.class));
+        deleteProduct.setOnClickListener(navigateTo(DeleteProductActivity.class));
+    }
+
+    public View.OnClickListener navigateTo(Class<?> cls) {
+        return (View v) -> {
+            final Intent intent = new Intent(v.getContext(), cls);
+            startActivity(intent);
+        };
     }
 }
